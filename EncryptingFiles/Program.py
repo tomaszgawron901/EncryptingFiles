ac394316d20dfd2e93a32b4438888ed14127ae3b
tomaszgawron901 <tomasz.gawron@microsoft.wsei.edu.pl>
2020-01-03 13:37:07 +0100

    Everything works fine, but really slow.

--- EncryptingFiles/Program.py
+++ EncryptingFiles/Program.py
@@ -9,29 +9,35 @@ class FileEncryptor:
     def __init__(self, algorithm):
         self.algorithm = algorithm
 
+    def ReadFile(self, path):
+        file = open(path, "rb")
+        out = file.read()
+        file.close()
+        return out
+
+    def WriteFile(self, bits, path):
+        file = open(path, "wb")
+        bits.tofile(file)
+        file.close()
+
     def Encrypt(self, path):
-        f = BitArray(open(path, "rb").read())
-        print(f.bin)
+        f = BitArray(self.ReadFile(path))
         out = BitArray()
         for i in range(0, f.__len__(), self.algorithm.DataSize):
             out += self.algorithm.Encrypt(f[i:i+self.algorithm.DataSize])
-        print(out.bin)
-        return out
+        self.WriteFile(out, path)
+
+    def Decrypt(self, path):
+        f = BitArray(self.ReadFile(path))
+        out = BitArray()
+        for i in range(0, f.__len__(), self.algorithm.DataSize):
+            out += self.algorithm.Decrypt(f[i:i+self.algorithm.DataSize])
+        self.WriteFile(out, path)
 
 
 def main():
-    s = FileEncryptor(Encryptor.TripleDES("4leyyfajnykey")).Encrypt("test.txt")
+    FileEncryptor(Encryptor.TripleDES("4leyyfajnykey")).Encrypt("test.txt")
+    FileEncryptor(Encryptor.TripleDES("4leyyfajnykey")).Decrypt("test.txt")
 
-    f = open("test.txt", "rb")
-    Bytes = f.read()
-    Bits= BitArray(Bytes)
-    print(Bits.__len__())
-    print(Bits[0:64].bin)
-    key = "4leyy"
-    out = Encryptor.TripleDES(key).Encrypt(Bits[0:64])
-    print(out.bin)
-    out = Encryptor.TripleDES(key).Decrypt(out)
-    print(out.bin)
-    f.close()
 
 main()
